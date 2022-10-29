ab6677d5e4e26b3149168249820da22425e49b5d
IM-J <S351945@oslomet.no>
2022-10-29 19:21:58 +0200

    Update ajax.js

--- wwwroot/js/ajax.js
+++ wwwroot/js/ajax.js
@@ -47,6 +47,9 @@ function hentSymptomer() {
         }).done((res) => {
             var IDList = document.querySelectorAll('input[name="1"]:checked').value;
             tidsint();
+            if (IDList.length = 30) {
+                swal("Det er litt mange symptomer, det er ikke sikkert du får et akkurat resultat. Om du opplever alle disse symptomene anbefales det å dra til legen og få tatt noen tester.");
+            }
         });
 }
 
